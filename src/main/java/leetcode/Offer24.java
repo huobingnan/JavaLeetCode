4b55ef2b2a6133a4ebb40bf3cbc64380bfc2478f
manan <75068951+huobingnan@users.noreply.github.com>
2023-02-01 13:56:11 +0800

    剑指 Offer 24. 反转链表

---
+++ src/main/java/leetcode/Offer24.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+import leetcode.runner.AC;
+import leetcode.runner.Outline;
+import leetcode.runner.ProblemLevel;
+
+@AC(time = "0 ms", memory = "41.2 MB", timeRank = "100%", memoryRank = "19.42%")
+@Outline(name = "剑指 Offer 24. 反转链表", description = "链表", level = ProblemLevel.EASY)
+public class Offer24 {
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null, ptr = head, next = null;
+        for (;;) {
+            if (ptr == null) break;
+            next = ptr.next;
+            ptr.next = prev;
+            prev = ptr;
+            ptr = next;
+        }
+        return prev;
+    }
+}
